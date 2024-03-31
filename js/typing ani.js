8f50824ba773e69e61ad04776ac3b75a5c82528d
KomalDahatonde <143823860+KomalDahatonde@users.noreply.github.com>
2024-03-31 10:11:54 +0530

    search removed

--- js/typing ani.js
+++ js/typing ani.js
@@ -1,29 +1,26 @@
 const words = ['Food items' , 'Clothes items', 'Electronic items']; // Add more words here if needed
-                                    let wordIndex = 0;
-                                    let letterIndex = 0;
-                                    let currentWord = '';
-                                    let typingSpeed = 100; // Adjust typing speed here
-                                
-                                    function type() {
-                                    if (letterIndex <= currentWord.length) {
-                                        document.getElementById('dynamic-text').textContent = currentWord.substring(0, letterIndex);
-                                        letterIndex++;
-                                        setTimeout(type, typingSpeed);
-                                    } else {
-                                        setTimeout(erase, 1000);
-                                    }
-                                    }
-                                
-                                    function erase() {
-                                    if (letterIndex >= 0) {
-                                        document.getElementById('dynamic-text').textContent = currentWord.substring(0, letterIndex);
-                                        letterIndex--;
-                                        setTimeout(erase, typingSpeed / 2);
-                                    } else {
-                                        wordIndex = (wordIndex + 1) % words.length;
-                                        currentWord = words[wordIndex]; // Convert word to uppercase
-                                        setTimeout(type, 500);
-                                    }
-                                    }
-                                
-                                    setTimeout(type, 1000);
+ let wordIndex = 0;
+ let letterIndex = 0;
+ let currentWord = '';
+ let typingSpeed = 100; // Adjust typing speed here  
+ function type() {
+ if (letterIndex <= currentWord.length) {
+     document.getElementById('dynamic-text').textContent = currentWord.substring(0, letterIndex);
+     letterIndex++;
+     setTimeout(type, typingSpeed);
+ } else {
+     setTimeout(erase, 1000);
+ }
+ }   
+ function erase() {
+ if (letterIndex >= 0) {
+     document.getElementById('dynamic-text').textContent = currentWord.substring(0, letterIndex);
+     letterIndex--;
+     setTimeout(erase, typingSpeed / 2);
+ } else {
+     wordIndex = (wordIndex + 1) % words.length;
+     currentWord = words[wordIndex]; // Convert word to uppercase
+     setTimeout(type, 500);
+ }
+ }   
+ setTimeout(type, 1000);
