27a77e6ccd78d017bea754cac370ac3d3c7acf7c
KomalDahatonde <143823860+KomalDahatonde@users.noreply.github.com>
2024-04-15 01:07:22 +0530

    nothing

--- js/razorpay_shop_page2.js
+++ js/razorpay_shop_page2.js
@@ -36,6 +36,7 @@ document.querySelectorAll('.paymentButton2').forEach(function(element) {
                         var order = {
                             paymentId: response.razorpay_payment_id,
                             productName: productName,
+                            amount: price,
                             address: add, // Include address in the order object
                             contact: contact, // Include contact number in the order object
                             status: 'success' // You can set the status as required
